c438f63644f46680a2aa6476eff2b74f761e913e
UmarAslamNawab <umar_uni321@outlook.com>
2020-03-29 20:46:33 +0500

    Class Files

---
+++ Coding Practices/index(29-3-20).js
@@ -0,0 +1,34 @@
+// alert("Javascript Session - UBIT")
+// alert("Umar Aslam")
+// alert("My Friends")
+// window.alert("A smarter way to learn JS")
+
+// var course = "Javascript Course"
+// alert(course)
+
+// var mobile = "LG G-6"
+// alert(mobile)
+
+// course = "Javascript UBIT"
+// alert(course)
+
+// var citizenship;
+// alert(citizenship) //undefined
+
+
+// var myname;
+// alert(myname)
+
+
+
+// myname = "Umar" 
+// alert(myname)
+
+
+
+var city = "Karachi"
+alert(city)
+
+var c = "Karachi"
+var city = "Karachi"
+var mycitynameis = "Karachi"
