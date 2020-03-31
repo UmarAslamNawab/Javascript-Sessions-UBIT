126071cfe55a52528e848905d503706f5b1f2c8a
UmarAslamNawab <umar_uni321@outlook.com>
2020-03-31 21:43:15 +0500

    31-3-20 file

---
+++ Coding Practices/index(31-3-20).js
@@ -0,0 +1,77 @@
+// var inc = 1
+// var newInc =inc + 1
+// alert(newInc) 
+
+
+// var inc = 1
+// inc++ 
+// alert(inc) 
+
+// var dec = 3
+// dec--
+// alert(dec)
+
+
+// var inc = 1
+// inc++
+// alert(inc)
+
+
+// var inc = 1
+// alert(inc++)  //old value  1 ===> 1+1=2
+// alert(inc)    //new value  2
+
+// var inc = 1
+// alert(++inc)   //new value  2
+// alert(inc)     //new value  2
+
+
+
+// var dec = 10 
+// dec--
+// alert(dec)
+
+
+// var dec2 = 8 
+// alert(dec2--)
+// alert(dec2)
+
+// var dec3 = 6
+// --dec3
+// alert(dec3)
+
+
+// var dec4 = 3
+// alert(--dec4)
+// alert(dec4)
+
+
+//post ++ -- > update the value in the same step but shows the older value if used
+//++ --  pre > update the value and also show the updated value in the same step
+
+
+// var a = 2;
+// var b = 1; 
+// var result = --a - --b + ++b + b--; // 1 - 0 + 1 + 1
+// alert(a); //1
+// alert(b); //0
+// // alert(result);
+
+
+
+// var totalCost = 8 / 2 * 2 / 4 * 4;
+// console.log(totalCost)
+
+
+// var resultOfComputation = ((2 * 4) * 4) + 2*3;
+// console.log(resultOfComputation)
+
+var name = "Umar"
+
+document.write("My name is " + name)
+
+
+//HOW JS performs maths
+// 1) Brackets
+// 2) Division/Multiplication
+// 3) Add/Sub
