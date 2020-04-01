0fb0335eb5ceb10ca00db9bb46f6c2af03594d72
UmarAslamNawab <umar_uni321@outlook.com>
2020-04-01 20:56:09 +0500

    updated JS file

---
+++ Coding Practices/index(1-4-20).js
@@ -0,0 +1,51 @@
+// var name  = "Umar"
+// alert("Thanks" + " " + name)
+
+var userName = "Umar"
+// var greeting = "Thanks"
+// var shoped = "Pants"
+// var finalRes = greeting + " " + userName + " " + "for Shopping" + " " + shoped
+// // alert(greeting + " " + userName + " " + "for Shopping" + " " + shoped)
+// alert(finalRes)
+
+
+// alert("78" + "3")
+
+// alert("2 plus 2 equals " + 2 + 2); //(2+2)
+
+
+// alert(userName)
+// console.log(userName)
+// document.write(userName)
+
+
+// var question = "What is your name?"
+// var myPrompt = prompt( question , userName)
+// console.log(myPrompt)
+
+
+// var myQues = "What is your age"
+// var defAns = 18
+// var ageThisYear = +prompt(myQues , defAns)  // "20" //20 //"Twenty"
+// var newAge = ageThisYear + 1 // 
+// console.log(newAge)
+
+
+// var a = 10
+
+// document.write("The value of a is " + a + "<br>")
+
+// document.write("The value of ++a is " + ++a + "<br>")
+
+// document.write("The value of a++ is " + a++ + "<br>")  //11 ==> 12
+
+// document.write("Now the value of a is " + a + "<br>")
+
+
+var subName = prompt("Enter your sub name?")
+var subMarks = prompt("Enter your " + subName +" Marks?")
+var totalMarks = 100
+
+
+
+document.write("SUBJECT : "+ subName + " " + "TOTAL MARKS: " +totalMarks + " " + "OBTAINED MARKS: " +subMarks + " " + "PERCENTAGE" + (subMarks*100)/100 + "%")
