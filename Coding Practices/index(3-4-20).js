4931fcdee9a10ee7d69f5602132d7ee0d91a9f4b
UmarAslamNawab <umar_uni321@outlook.com>
2020-04-03 21:18:23 +0500

    modified files

---
+++ Coding Practices/index(3-4-20).js
@@ -0,0 +1,112 @@
+
+// if(matches the condition){
+//    if the () condition wents true the execute this block
+// }
+
+// var question = prompt("Where did you live")
+
+// if(question === "Karachi" ){
+//     alert("Welcome To Karachi")
+// }
+
+
+// var myCity = "Karachi"
+
+// if(myCity === "Karachi"){
+//     alert("WELCOME")
+// }
+
+
+// var userIq = "Dull"
+// var score = 9
+
+// alert(userIq)
+// alert(score)
+
+// var city = prompt("What is your city name?") //Lahore
+
+// if(city !== "Karachi"){
+//     userIq = "Genius"
+//     score++
+//     alert("correct")
+// }
+
+// alert(userIq)
+// alert(score)
+
+
+// var number = 5
+// var count = 0
+
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") //document.write(number + " "+ "X " + "1" + " =" + " " + number*1+ "<br>") 
+
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+// document.write(number + " "+ "X " + ++count + " =" + " " + number*count+ "<br>") 
+
+
+
+// var color = prompt("Enter color of road traffic signal");
+
+// if (color === "red") {
+//   alert("Must Stop");
+// }
+
+// if (color === "yellow") {
+//   alert("Ready to move");
+// }
+
+// if (color === "green") {
+//   alert("Move now");
+// }
+
+
+// var a = 4;
+
+// if (++a === 5) {
+//   alert("given condition for variable is true");  //ok
+// }
+
+// var b = 82;
+// if (b++ === 83) {
+//   alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13) {
+//   alert("condition 1 is true");
+// }
+// if (c === 13) {
+//   alert("condition 2 is true"); //ok
+// }
+// if (++c < 14) {
+//   alert("condition 3 is true");
+// }
+// if (c === 14) {
+//   alert("condition 4 is true"); //ok
+// }
+
+
+
+// if ("car" < "cat") {
+//   alert("Car is Smaller than Cat");
+// }
+
+
+
+
+var num = +prompt("Enter Your Number");
+
+if (num % 2 !== 0) {       
+  alert("Odd Number");
+}
+
+if (num % 2 == 0) {      
+  alert("Even Number");
+}
