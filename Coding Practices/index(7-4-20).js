c1771311f24fd99faeec739317557c54878d0450
UmarAslamNawab <umar_uni321@outlook.com>
2020-04-07 20:59:57 +0500

    new modified files

---
+++ Coding Practices/index(7-4-20).js
@@ -0,0 +1,163 @@
+// var weight = +prompt("enter weight?")
+// var time = +prompt("enter time?")
+// var age = +prompt("enter age?")
+// var gender = +prompt("enter your gender?")
+
+
+// if (weight > 300 || time < 6 || age > 17 || gender === "male") {
+// alert("Come to our tryout!");
+// }
+// else {
+// alert("Come to our cookout!");
+// }
+
+
+
+// var country = prompt("What is your country?")
+// var city= prompt("What is your city?")
+// var district= prompt("what is your district?")
+
+// if(country === "Pakistan"){
+//     if(city === "karachi"){
+//         if(district === "central"){
+//             alert("Your district office is located at north nazimabad town kindly contact the managment")
+//         }
+//         else if(district === "south"){
+//             alert("Your district office is located at garden south town kindly contact the managment")
+
+//         }
+//         else if(district === "east"){
+//             alert("Your district office is located at garden east town kindly contact the managment")
+
+//         }
+//         else{
+//             alert("You are not a part of Karachi City")
+//         }
+// }
+//  else if(city === "lahore"){
+//     if(district === "central"){
+//         alert("Your district office is located at raja bazar town kindly contact the managment")
+//     }
+//     else if(district === "south"){
+//         alert("Your district office is located at model town kindly contact the managment")
+
+//     }
+//     else if(district === "east"){
+//         alert("Your district office is located at DHA town kindly contact the managment")
+
+//     }
+//     else{
+//         alert("You are not a part of Lahore City")
+//     }
+    
+//  }
+// }
+// else if((country === "US") && (city=== "texas" || city === "houston") && (district === "south" || district === "east" || district === "central")){
+//    alert("KIndly contact your " + district + " " +  "mobile phone no")
+// }
+ 
+
+
+
+// else{
+//  alert("You are not eligilble")   
+// }
+
+
+// var age = 50
+// var res= "US"
+
+// if ((age > 65 || age < 21) && res === "U.S.") {
+// alert("CONGO")
+// }
+// else{
+//     alert("Sorry")
+// }
+
+
+
+
+/** Logical operators **/
+
+        // AND - &&
+        // OR - ||
+        // NOT - !
+
+        //        true && true   = true
+        //        false && true  = false
+        //        false && false = false
+        //        true && false  = false
+
+        //true || true   = true
+        //false || true  = true
+        //false || false = false
+        //true || false  = true
+
+
+/** Ternary Operator **/
+
+
+        // var isConfirmed = true;
+        // var status;
+        // if (isConfirmed == true) {
+        //     status = "Confirmed";
+        // }
+        // else {
+        //     status = "Pending";
+        // }
+        // alert(status)
+
+
+        // var isConfirmed = true;
+        // var status;
+        // status = isConfirmed == true ? "Confirmed" : "Pending";
+        // alert(status)        
+
+
+
+        
+// /** Testing multiple conditions in one statement **/
+
+        // var test = "JS";
+        // var score = 65;
+
+        // if (test === "CSS" && score > 60) {
+        //     alert("You have passed CSS test");
+        // }
+        // if (test === "JS" && score > 70) {
+        //     alert("You have passed JS test");
+        // }
+        // else{
+        //     alert("Sorry You have the record in our system")
+        // }
+
+
+
+        // var age = 23;
+        // var residence = "UK";
+        // var IELTSScore = 8.2;
+
+        // if(age > 20 || residence == "US"){
+        //     alert("You can proceed");
+        // }
+
+        // if(age > 20 || (residence == "US" && IELTSScore > 8.0)){
+        //     alert("You can proceed");
+        // }
+
+
+
+
+        // var time = parseInt(prompt("Enter time in 24 hour format E.g. 1300 for 1pm", "1300"));
+        // if (time >= 0000 && time < 1200) {
+        //     message = "Good morning!";
+        // } else if (time >= 1200 && time < 1700) {
+        //     message = "Good afternoon!";
+        // } else if (time >= 1700 && time < 2100) {
+        //     message = "Good evening!";
+        // } else if (time >= 2100 && time <= 2359) {
+        //     message = "Good night!";
+        // }
+        // alert(message);
+
+
